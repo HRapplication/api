cc0bc9e7dfdec1435da84b5a4e63963269f9be1c
tibetansandfox <pawel.zyb@gmail.com>
2017-05-19 18:00:46 +0200

    added column is_available to job_offers

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515134811) do
+ActiveRecord::Schema.define(version: 20170519155936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,8 +138,9 @@ ActiveRecord::Schema.define(version: 20170515134811) do
   create_table "job_offers", force: :cascade do |t|
     t.string   "title"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.boolean  "is_available"
   end
 
   add_index "job_offers", ["user_id"], name: "index_job_offers_on_user_id", using: :btree
