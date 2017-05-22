5a9df54df8d80e8c3cb0ca01232b884e11c683c4
Adam <adam.maziarz39@gmail.com>
2017-05-22 21:16:18 +0200

    login, register templates added

---
+++ app/assets/javascripts/controllers/events_ctrl.js
@@ -0,0 +1,22 @@
+
+angular.module('HrApp')
+    .controller('eventsCtrl', function($scope) {
+        $scope.events = [
+            {
+                name: 'Wydarzenie 1',
+                content: 'Minim eiusmod exercitation ad aliqua non aliqua ea. Cillum eiusmod dolor consequat elit pariatur culpa esse non fugiat amet et. Id dolor consequat dolore sit cupidatat eu sunt veniam consectetur aliqua velit. Laboris voluptate aliqua incididunt magna aliqua sint tempor Lorem sunt eu. Nulla ipsum eiusmod labore ex in. Velit consectetur occaecat nisi eiusmod laboris ex nisi et pariatur voluptate.'
+            },
+            {
+                name: 'Wydarzenie 2',
+                content: 'Minim eiusmod exercitation ad aliqua non aliqua ea. Cillum eiusmod dolor consequat elit pariatur culpa esse non fugiat amet et. Id dolor consequat dolore sit cupidatat eu sunt veniam consectetur aliqua velit. Laboris voluptate aliqua incididunt magna aliqua sint tempor Lorem sunt eu. Nulla ipsum eiusmod labore ex in. Velit consectetur occaecat nisi eiusmod laboris ex nisi et pariatur voluptate.'
+            },
+            {
+                name: 'Wydarzenie 3',
+                content: 'Minim eiusmod exercitation ad aliqua non aliqua ea. Cillum eiusmod dolor consequat elit pariatur culpa esse non fugiat amet et. Id dolor consequat dolore sit cupidatat eu sunt veniam consectetur aliqua velit. Laboris voluptate aliqua incididunt magna aliqua sint tempor Lorem sunt eu. Nulla ipsum eiusmod labore ex in. Velit consectetur occaecat nisi eiusmod laboris ex nisi et pariatur voluptate.'
+            },
+            {
+                name: 'Wydarzenie 4',
+                content: 'Minim eiusmod exercitation ad aliqua non aliqua ea. Cillum eiusmod dolor consequat elit pariatur culpa esse non fugiat amet et. Id dolor consequat dolore sit cupidatat eu sunt veniam consectetur aliqua velit. Laboris voluptate aliqua incididunt magna aliqua sint tempor Lorem sunt eu. Nulla ipsum eiusmod labore ex in. Velit consectetur occaecat nisi eiusmod laboris ex nisi et pariatur voluptate.'
+            },
+        ];
+    });
