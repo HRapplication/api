83bda7e82cfa483146b53dbc28d935d0b6ed7db1
Bartosz Tomczyk <b.tomczyk.wroc@gmail.com>
2017-05-29 23:30:24 +0200

    forms list added

--- app/assets/javascripts/controllers/forms_ctrl.js
+++ app/assets/javascripts/controllers/forms_ctrl.js
@@ -1,17 +1,32 @@
 angular.module('HrApp') 
-    .controller('formsCtrl', function($scope, $http) { 
+    .controller('formsCtrl', function($scope, $http, $filter) { 
          
         $scope.waiting_forms = [];
         $scope.rejected_forms = []; 
         $scope.accepted_forms = []; 
-        $scope.waitingTitles = [];
+        $scope.wSickForms = [];
+        
     
  
         $http.get('/forms/combined', {params: {status: 'waiting'}}). 
             success(function(data) { 
                 $scope.waiting_forms = data;                 
                 console.log($scope.waiting_forms); 
-                angular.forEach($scope.waiting_forms, viewTitle);
+                angular.forEach($scope.waiting_forms, function(value, key) {
+                    if(value.name == 'sick_leave_form'){
+                        value.newName = 'Urlop zdrowotny';
+                    }
+                    else if(value.name == 'business_trip_form'){
+                        value.newName = 'Delegacja';
+                    }
+                    else if(value.name == 'holiday_form'){
+                        value.newName = 'Urlop';
+                    }
+                    else if(value.name == 'homeoffice_form'){
+                        value.newName = 'Praca zdalna';
+                    }
+                });
+                console.log($scope.wSickForms);
             }). 
              error(function(data) { 
                 console.log("Error!!!"); 
@@ -20,7 +35,21 @@ angular.module('HrApp')
         $http.get('/forms/combined', {params: {status: 'rejected'}}). 
             success(function(data) { 
                 $scope.rejected_forms = data;                 
-                console.log($scope.rejected_forms); 
+                console.log($scope.rejected_forms);
+                angular.forEach($scope.rejected_forms, function(value, key) {
+                    if(value.name == 'sick_leave_form'){
+                        value.newName = 'Urlop zdrowotny';
+                    }
+                    else if(value.name == 'business_trip_form'){
+                        value.newName = 'Delegacja';
+                    }
+                    else if(value.name == 'holiday_form'){
+                        value.newName = 'Urlop';
+                    }
+                    else if(value.name == 'homeoffice_form'){
+                        value.newName = 'Praca zdalna';
+                    }
+                }); 
             }). 
              error(function(data) { 
                 console.log("Error!!!"); 
@@ -29,7 +58,22 @@ angular.module('HrApp')
         $http.get('/forms/combined', {params: {status: 'accepted'}}). 
             success(function(data) { 
                 $scope.accepted_forms = data;                 
-                console.log($scope.accepted_forms); 
+                console.log($scope.accepted_forms);
+                angular.forEach($scope.accepted_forms, function(value, key) {
+                    if(value.name == 'sick_leave_form'){
+                        value.newName = 'Urlop zdrowotny';
+                    }
+                    else if(value.name == 'business_trip_form'){
+                        value.newName = 'Delegacja';
+                    }
+                    else if(value.name == 'holiday_form'){
+                        value.newName = 'Urlop';
+                    }
+                    else if(value.name == 'homeoffice_form'){
+                        value.newName = 'Praca zdalna';
+                    }
+                });
+                
             }). 
              error(function(data) { 
                 console.log("Error!!!"); 
@@ -44,25 +88,31 @@ angular.module('HrApp')
            };  
         $scope.showRejected = function(clickEvent){ 
                $('.btn-group > .btn').removeClass('active'); 
-               $('.btn-group > .btn').eq(0).addClass('active'); 
+               $('.btn-group > .btn').eq(1).addClass('active'); 
                $("#waiting_forms").hide(); 
                $("#rejected_forms").show(); 
                $("#accepted_forms").hide();                 
            }; 
         $scope.showAccepted = function(clickEvent){ 
                $('.btn-group > .btn').removeClass('active'); 
-               $('.btn-group > .btn').eq(0).addClass('active'); 
+               $('.btn-group > .btn').eq(2).addClass('active'); 
                $("#waiting_forms").hide(); 
                $("#rejected_forms").hide(); 
                $("#accepted_forms").show();                 
             }; 
 
-        function viewTitle() {
-            if(this.hasOwnProperty('care_type'))
-            {
-                $scope.title = 'Urlop zdrowotny';
-                console.log($scope.title);
+        /*function viewTitle() {
+
+            var titles = [];
+
+            for(i=0; i<this.length; i++){
+                if(this[i].name=="leave_sick_form"){
+                    titles[i] = 'Urlop zdrowotny';
+                    console.log(titles[i]);
+                }
             }
-        }
+
+            return titles;
+        }*/
              
   });
--- app/assets/javascripts/controllers/my_forms_ctrl.js
+++ app/assets/javascripts/controllers/my_forms_ctrl.js
@@ -1,18 +1,41 @@
 angular.module('HrApp') 
     .controller('myFormsCtrl', function($scope, $http, $filter) { 
          
-        $scope.forms = [];  
- 
-         
-        $http.get('/forms/user_combined'). 
+        $scope.forms = [];
+      
+       
+         $http.get('/forms/user_combined', {params: {status: 'waiting'}} || {params: {status: 'rejected'}} || {params: {status: 'accepted'}}  ). 
             success(function(data) { 
-                $scope.forms = data;  
-               console.log($scope.forms); 
+                $scope.forms = data;                 
+                console.log($scope.forms); 
+                angular.forEach($scope.forms, function(value, key) {
+                    if(value.name == 'sick_leave_form'){
+                        value.newName = 'Urlop zdrowotny';
+                    }
+                    else if(value.name == 'business_trip_form'){
+                        value.newName = 'Delegacja';
+                    }
+                    else if(value.name == 'holiday_form'){
+                        value.newName = 'Urlop';
+                    }
+                    else if(value.name == 'homeoffice_form'){
+                        value.newName = 'Praca zdalna';
+                    }
+                    if(value.status == 'waiting'){
+                        value.newStatus = 'Oczekujący';
+                    }
+                    if(value.status == 'rejected'){
+                        value.newStatus = 'Niezaakceptowany';
+                    }
+                    if(value.status == 'accepted'){
+                        value.newStatus = 'Zaakceptowany';
+                    }
+                });
             }). 
              error(function(data) { 
                 console.log("Error!!!"); 
             }); 
-               
              
     }); 
 
+  
