c4844fae8b334a999c2ad74d0790952f8ec0407f
tibetansandfox <pawel.zyb@gmail.com>
2017-05-29 22:42:22 +0200

    updated admin

--- app/admin/job_offer.rb
+++ app/admin/job_offer.rb
@@ -1,23 +1,26 @@
 ActiveAdmin.register JobOffer do
 
-permit_params :title, :user_id
+permit_params :title, :is_available, :user_id
 
   index do
     selectable_column
     id_column
     column :title
+    column :is_available
     column :user_id
     column :created_at
     actions
   end
 
   filter :title
+  filter :is_available
   filter :user_id
   filter :created_at
 
   form do |f|
     f.inputs "Job offer details" do
       f.input :title
+      f.input :is_available
       f.input :user_id
     end
     f.actions
