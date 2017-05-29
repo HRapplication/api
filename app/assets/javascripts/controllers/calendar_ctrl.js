9fefffea13f32dd4995f7ec703de68e9098f3188
Adam <adam.maziarz39@gmail.com>
2017-05-29 17:33:15 +0200

    calendars mod

--- app/assets/javascripts/controllers/calendar_ctrl.js
+++ app/assets/javascripts/controllers/calendar_ctrl.js
@@ -1,5 +1,15 @@
 angular.module('HrApp')
-    .controller('CalendarCtrl', function($scope) {
+    .controller('CalendarCtrl', function($scope, $http) {
+
+    $scope.events = {};
+    
+
+    $http.get('/schedules', {params: {start_date: '2017-05-29', end_date:'2017-05-30'}}).
+    success(function(data){
+        console.log(data);
+        $scope.events = data;
+    })
+
         $(document).ready(function() {
 
     // page is now ready, initialize the calendar...
@@ -15,24 +25,18 @@ angular.module('HrApp')
       editable: true,
       eventLimit: true,      
       events: [
-                                {
-                                    title: 'Praca',
-                                    start: '2017-05-29T07:00:00',
-                                    end: '2017-05-29T16:00:00'
-                                },
-                                {
-                                    title: 'Praca',
-                                    start: '2017-05-30T12:00:00',
-                                    end: '2017-05-30T16:00:00'
-                                },
-                                {
-                                    title: 'Praca',
-                                    start: '2017-05-31T07:00:00',
-                                    end: '2017-05-31T16:00:00'
-                                },
-                                
-                           
-                            ]                   
+                    {
+                        //title: 'Praca',
+                        start: $scope.events[0].work_date+$scope.events[0].start_hour.substr(11,19),
+                        end: $scope.events[0].work_date+$scope.events[0].end_hour.substr(11,19)
+                    },
+                    {
+                        //title: 'Praca',
+                        start: '2017-05-29T10:00:00',
+                        end: '2017-05-29T16:00:00'
+                    },
+                
+                ]                   
      
         });
 
