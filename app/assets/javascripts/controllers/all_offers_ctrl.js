ca3f24a29e5c3cbdd5b3efd80b7fdca3b164e2f1
Bartosz Tomczyk <b.tomczyk.wroc@gmail.com>
2017-06-13 19:49:12 +0200

    Offersin login added

--- app/assets/javascripts/controllers/all_offers_ctrl.js
+++ app/assets/javascripts/controllers/all_offers_ctrl.js
@@ -1,5 +1,32 @@
 angular.module('HrApp')
-    .controller('allOffersCtrl', function($scope, $http, $state) {
-      console.log("wszystkie oferty")
+    .controller('allOffersCtrl', function($scope, $http) {
 
-    })
+        $scope.availableOffers = {};
+        $scope.disavailableOffers = {};
+
+
+        $http.get('/offers', {params: {is_available: true}}).
+            success(function(data) {
+                $scope.availableOffers = data;
+                angular.forEach($scope.availableOffers, function(value, key) {
+                    if(value.is_available == true){
+                        value.isAvailable = 'Oferta dostępna';
+                    }
+                });                
+            }).
+            error(function(data) {
+                console.log("Error!!!");
+            });
+
+        function showModal(modalId) {
+            $(modalId).modal('show');
+            $(modalId).on('hide.bs.modal', reloadView);
+        }
+
+        function reloadView() {
+            $('body').removeClass('modal-open').css('padding-right', '');
+            $('.modal-backdrop').remove();       
+            $state.reload();
+        }
+            
+    });
